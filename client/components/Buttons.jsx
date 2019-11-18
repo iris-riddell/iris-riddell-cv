6a89a8a94abcc8573f0301c5a3d60debbc8856f9
Iris Riddell <riddell.iris@gmail.com>
2019-11-18 13:05:58 +1300

    fixed z-index bug

--- client/components/Buttons.jsx
+++ client/components/Buttons.jsx
@@ -6,9 +6,9 @@ const Buttons = () => {
   return (
 <div className='buttons'>
     <Link to='/portfolio' style={{padding: '1rem'}}>Portfolio</Link>
-    <Link to='/workhistory' style={{padding: '1rem'}}>Work History</Link>
-    <Link to='/skills' style={{padding: '1rem'}}>Skills</Link>
-    <Button>Click Here</Button>
+    <Button><Link to='/workhistory' style={{padding: '1rem'}}>Work History</Link></Button>
+    <Button><Link to='/skills' style={{padding: '1rem'}}>Skills</Link></Button>
+    
     </div>
       )
     }
