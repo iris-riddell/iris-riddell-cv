717dd66e582236802d5e37c896ef9ed0a2540ff6
Iris Riddell <riddell.iris@gmail.com>
2019-11-18 16:02:47 +1300

    Added custom fonts

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => (
   <div className='header'>
-    <h1 style={{fontSize: '3.5rem', margin: '0'}} >Iris Riddell</h1>
-    <h2 style={{fontSize: '2rem', margin: '0'}}>Web Developer & Content Creator</h2>
+    <h1 style={{fontFamily: 'Bebas Neue', fontSize: '3.5rem', margin: '0'}} >Iris Riddell</h1>
+    <h2 style={{fontFamily: 'Bebas Neue', fontSize: '2rem', margin: '0'}}>Web Developer & Content Creator</h2>
   </div>
 )
 
--- client/components/HomepageText.jsx
+++ client/components/HomepageText.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HomepageText = () => (
   <div className='homepage-text'>
-    <h2>Hello, my name is Iris.</h2>
-    <p>I am a creative, hard-working web developer with a background in journalism and communications. I have an interest in the power of community, and a deep desire to build websites and tell stories that connect, educate and inspire. </p>
+    <h2 style={{fontFamily: 'Slabo+27px'}}>Hi, I'm Iris.</h2>
+    <p style={{fontFamily: 'Quicksand'}}>I am a creative, hard-working web developer with a background in journalism and communications. I have an interest in the power of community, and a deep desire to build websites and tell stories that connect, educate and inspire. </p>
   </div>
 )
 
